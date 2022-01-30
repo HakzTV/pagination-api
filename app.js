17a98a89afdd56dd6254caf9519d5119568ea9d0
Telvin Varfley <telvinvarfley@gmail.com>
2022-01-30 13:47:47 +0000

    Local Database added

--- app.js
+++ app.js
@@ -1,69 +1,52 @@
+
+// New
 const express = require("express")
 const bodyParser = require("body-parser")
-
+const mongoose = require("mongoose")
 const app = express()
 
 const port = process.env.PORT|| 3000
 
 app.use(bodyParser.urlencoded({extended: true}))
 
+// DB Connection
+const url = "mongodb://localhost:27017/paginationDB";
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true 
+} 
+ mongoose.connect(url, options)
 
-const users = [
-    {id: 1 , name: 'User 1'},
-    {id: 2 , name: 'User 2'},
-    {id: 3 , name: 'User 3'},
-    {id: 4 , name: 'User 4'},
-    {id: 5 , name: 'User 5'},
-    {id: 6 , name: 'User 6'},
-    {id: 7 , name: 'User 7'},
-    {id: 8 , name: 'User 8'},
-    {id: 9 , name: 'User 9'},
-    {id: 10 , name: 'User 10'},
-    {id: 11 , name: 'User 11'},
-    {id: 12 , name: 'User 12'},
-    {id: 13 , name: 'User 13'},
-    {id: 14 , name: 'User 14'},
-    {id: 15 , name: 'User 15'},
-    {id: 16 , name: 'User 16'},
-    {id: 17 , name: 'User 17'},
-    {id: 18 , name: 'User 18'},
-    {id: 19, name: 'User 19'},
-]
-const posts = [
-    {id: 1 , name: 'Post 1'},
-    {id: 2 , name: 'Post 2'},
-    {id: 3 , name: 'Post 3'},
-    {id: 4 , name: 'Post 4'},
-    {id: 5 , name: 'Post 5'},
-    {id: 6 , name: 'Post 6'},
-    {id: 7 , name: 'Post 7'},
-    {id: 8 , name: 'Post 8'},
-    {id: 9 , name: 'Post 9'},
-    {id: 10 , name: 'Post 10'},
-    {id: 11 , name: 'Post 11'},
-    {id: 12 , name: 'Post 12'},
-    {id: 13 , name: 'Post 13'},
-    {id: 14 , name: 'Post 14'},
-    {id: 15 , name: 'Post 15'},
-    {id: 16 , name: 'Post 16'},
-    {id: 17 , name: 'Post 17'},
-    {id: 18 , name: 'Post 18'},
-    {id: 19, name: 'Post 19'},
-]
-
-app.get('/post', paginatedResults(posts), (req, res)=>{
-    res.json(res.paginatedResults)
-    
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    }
 })
+const User = new mongoose.model("User", userSchema)
 
-app.get('/users', paginatedResults(users), (req,res)=>{
+// Get Route
+app.get('/users', paginatedResults(User), (req,res)=>{
     // So it can return numbers instead of a string
     res.json(res.paginatedResults)
 })
 
-// Middleware 
+// Requests are handled here
+app.post('/users',(req,res)=>{
+    const users = new User({
+        name: req.body.name
+    })
+    users.save((err)=>{
+        if(!err){
+            console.log(err)
+             console.log("User has been added")
+            res.redirect("/users")
+        }
+    })
+})
+// Middleware function
 function paginatedResults(model){
-    return(req,res, next)=>{
+    return async (req,res, next)=>{
     // So it can return numbers instead of a string
 
     const page = parseInt(req.query.page);
@@ -78,7 +61,7 @@ function paginatedResults(model){
 
     const results = {}
 
-    if(endIndex < model.length){
+    if(endIndex < await model.countDocuments().exec()){
         results.next = {
             page: page + 1,
             limit: limit 
@@ -92,9 +75,17 @@ function paginatedResults(model){
         }
     }
   
-   results.results = model.slice(startIndex, endIndex)
-   res.paginatedResults=  results
-   next()
+    // This replaces the slice
+    try{
+        results.results = await model.find().limit(limit).skip(startIndex).exec()
+        res.paginatedResults=  results
+
+        next()
+    }catch(e){
+        res.status(500).json({message : e.message})
+    }
+     
+  
     }
 }
 app.listen(port, ()=>{
